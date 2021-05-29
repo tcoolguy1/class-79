4b65f751706c71dfc2b8a1f7b60f2f2848f1c718
tcoolguy1 <78965454+tcoolguy1@users.noreply.github.com>
2021-05-29 19:00:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+name_of_the_student_array = [];
+
+function submit()
+{
+    var name_1 =document.getElementById("name_of_the_student_1" ).value;
+    var name_2 =document.getElementById("name_of_the_student_2" ).value;
+    var name_3 =document.getElementById("name_of_the_student_3" ).value;
+    var name_4 =document.getElementById("name_of_the_student_4" ).value;
+
+    name_of_the_student_array.push(name_1);
+    name_of_the_student_array.push(name_2);
+    name_of_the_student_array.push(name_3);
+    name_of_the_student_array.push(name_4);
+document.getElementById("display_name").innerHTML= name_of_the_student_array;
+document.getElementById("sumbit_button").style.display="none";
+document.getElementById("sumbit_button").style.display="inline-block";
+
+}
+function sorting()
+{
+    name_of_the_student_array.sort();
+    document.getElementById("display_name").innerHTML= name_of_the_student_array;
+}
